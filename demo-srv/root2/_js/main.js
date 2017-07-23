d4feaa1a06cce434a4b6bd25b55d7671bff085e8
Wolfgang <wgehner@gmail.com>
2017-07-23 08:08:36 -0700

    Doc

--- demo-srv/root2/_js/main.js
+++ demo-srv/root2/_js/main.js
@@ -9,9 +9,9 @@ var TM = {
 			TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/vendor/jquery.jsForm.min.js')
 			, TS.load('//cdn.jsdelivr.net/dot.js/1.1.1/doT.min.js')
 			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/webComps/tw0-1.0.js').then(function(){TW.init()}) //Standard Polyfills and Helper
-			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/webComps/tp1-1.0.js') //Late-loader for Polymer components
+			, TS.load('//cdn.rawgit.com/topseed/topseed-turbo/master/webComps/tp1-1.0.js') //Late-loader Helper for Polymer components
 			, TS.load('/bower_components/webcomponentsjs/HTMLImports.min.js') //Polyfill used in late-loader
-			, TS.load('/bower_components/polymer/polymer.html') //Support for Polymer 1
+			, TS.load('/bower_components/polymer/polymer.html') //Support for Polymer (1.9.3)
 			//, TS.load('//cdn.jsdelivr.net/riot/3.4.4/riot+compiler.min.js') //Support for RIOT
 			, TS.load('//rawgit.com/topseed/topseed-turbo/master/release/topseed-turbo-latest.js')
 			, TS.load('/_js/BLX.js')
